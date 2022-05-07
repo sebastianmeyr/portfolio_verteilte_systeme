dd2d35f32cf2b2f4bf8235d9186593b39cb66d4e
sebastianmeyr <sebastianmeyr@gmx.net>
2022-05-07 16:55:50 +0200

    Implemented Review List View
    Co-authored-by: juliapfanger <78020408+juliapfanger@users.noreply.github.com>

--- Frontend/src/page-details/page-details.js
+++ Frontend/src/page-details/page-details.js
@@ -40,41 +40,46 @@ export default class PageDetails extends Page {
         this._mainElement.innerHTML = html;
 
               
+        let reviews = await this._app.backend.fetch("GET", "/productReviews/" + this._dataset.number);
+        this._emptyMessageElement = this._mainElement.querySelector(".empty-placeholder");
 
-        /*this._emptyMessageElement = this._mainElement.querySelector(".empty-placeholder");
-
-        if (data.length) {
+        if (reviews.length) {
             this._emptyMessageElement.classList.add("hidden");
-        }*/
+        }
 
         // Je Datensatz einen Li der Produktbewertungensteneintrag generieren
-        /*let olElement = this._mainElement.querySelector("ol");
+        let ulElement = this._mainElement.querySelector("ul");
 
         let templateElement = this._mainElement.querySelector(".list-entry");
         let templateHtml = templateElement.outerHTML;
         templateElement.remove();
 
-        for (let index in data) {
+        for (let index in reviews) {
             // Platzhalter ersetzen
-            let dataset = data[index];
+            let review = reviews[index];
             let html = templateHtml;
 
-            html = html.replace("$ID$", dataset._id);
-            html = html.replace("$NAME$", dataset.name);
-            html = html.replace("$PICTURE_URL$", dataset.picture_url);
-            html = html.replace("$LIKE$", dataset.like);
-            html = html.replace("$DISLIKE$", dataset.dislike);
+            html = html.replace("$NAME$", review.name);
+
+            let pictureUrl;
+            if (review.like)
+                pictureUrl = "https://previews.123rf.com/images/mutlik/mutlik1808/mutlik180800021/112002464-cartoon-wie-daumen-hoch-im-comic-stil-vektor-illustration-.jpg";
+            else
+                pictureUrl = "https://previews.123rf.com/images/triken/triken1608/triken160800024/61320639-dislike-smileyemoticon-unterzeichnen-vektor-illustration-auf-wei%C3%9Fem-hintergrund-.jpg";
+
+            html = html.replace("$PICTURE_URL$", pictureUrl);
+            html = html.replace("$COMMENT$", review.comment);
 
             // Element in die Liste einfügen
             let dummyElement = document.createElement("div");
             dummyElement.innerHTML = html;
             let liElement = dummyElement.firstElementChild;
             liElement.remove();
-            olElement.appendChild(liElement);
+            ulElement.appendChild(liElement);
 
             // Event Handler registrieren
             //liElement.querySelector(".action-click").addEventListener("click", () => location.hash = `#/${dataset._id}`);
-        }*/
+        }
 
         //// TODO: Anzuzeigende Inhalte laden mit this._app.backend.fetch() ////
         //// TODO: Inhalte in die HTML-Struktur einarbeiten ////
